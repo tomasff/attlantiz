02cdf0f7e88db7793df9c3ea41d92bfe0126a07b
Tomás F <tomasff02@gmail.com>
2022-06-24 23:18:51 +0100

    Setup unit testing with JUnit

---
+++ src/test/java/com/tomff/attlantiz/DiskStoreTests.java
@@ -0,0 +1,14 @@
+package com.tomff.attlantiz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DiskStoreTests {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+
+}
