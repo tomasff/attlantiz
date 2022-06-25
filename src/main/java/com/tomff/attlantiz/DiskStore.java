b00999eb04469f6a020848701c4eab5225a0846f
Tomás F <tomasff02@gmail.com>
2022-06-25 14:45:28 +0100

    Add tests for the disk-based store

--- src/main/java/com/tomff/attlantiz/DiskStore.java
+++ src/main/java/com/tomff/attlantiz/DiskStore.java
@@ -1,7 +1,37 @@
 package com.tomff.attlantiz;
 
+import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Optional;
 
-public class DiskStore {
+public class DiskStore implements Store {
 
+    private final Path file;
+
+    public DiskStore(Path file) {
+        this.file = Objects.requireNonNull(
+                file,
+                "The file path for an Attlantiz DiskStore must not be null."
+        );
+    }
+
+    @Override
+    public void put(String key, String value) {
+
+    }
+
+    @Override
+    public Optional<String> get(String key) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<String> remove(String key) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
--- src/test/java/com/tomff/attlantiz/DiskStoreTests.java
+++ src/test/java/com/tomff/attlantiz/DiskStoreTests.java
@@ -1,14 +1,121 @@
 package com.tomff.attlantiz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiskStoreTests {
 
-    @Test
-    public void dummyTest() {
-        assertTrue(true);
+    @TempDir
+    private Path temporaryStoreDir;
+
+    private DiskStore buildDiskStore() {
+        return new DiskStore(temporaryStoreDir.resolve("store.attz"));
     }
 
+    @Test
+    public void putAndGet() {
+        DiskStore store = buildDiskStore();
+
+        store.put("key", "value");
+
+        assertEquals(Optional.of("value"), store.get("key"));
+        store.close();
+    }
+
+
+    @Test
+    public void putAndGetMultiple() {
+        DiskStore store = buildDiskStore();
+
+        for (int i = 0; i < 1000; i++) {
+            store.put("key" + i, "value" + i);
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            assertEquals(Optional.of("value" + i), store.get("key" + i));
+        }
+
+        store.close();
+    }
+
+    @Test
+    public void getNonexistentKeyValue() {
+        DiskStore store = buildDiskStore();
+
+        assertEquals(Optional.empty(), store.get("key"));
+        store.close();
+    }
+
+    @Test
+    public void putAndGetAndRemove() {
+        DiskStore store = buildDiskStore();
+
+        store.put("key", "value");
+
+        assertEquals(Optional.of("value"), store.get("key"));
+        assertEquals(Optional.of("value"), store.remove("key"));
+        assertEquals(Optional.empty(), store.get("key"));
+
+        store.close();
+    }
+
+    @Test
+    public void putAndGetAndRemoveMultiple() {
+        DiskStore store = buildDiskStore();
+
+        for (int i = 0; i < 1000; i++) {
+            store.put("key" + i, "value" + i);
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            assertEquals(Optional.of("value" + i), store.get("key" + i));
+            assertEquals(Optional.of("value" + i), store.remove("key" + i));
+            assertEquals(Optional.empty(), store.get("key" + i));
+        }
+
+        store.close();
+    }
+
+    @Test
+    public void putIsPersistent() {
+        DiskStore store = buildDiskStore();
+
+        store.put("key", "value");
+        assertEquals(Optional.of("value"), store.get("key"));
+
+        store.close();
+
+        store = buildDiskStore();
+        assertEquals(Optional.of("value"), store.get("key"));
+
+        store.close();
+    }
+
+    @Test
+    public void putAndRemoveIsPersistent() {
+        DiskStore store = buildDiskStore();
+
+        store.put("key", "value");
+        assertEquals(Optional.of("value"), store.get("key"));
+
+        store.close();
+
+        store = buildDiskStore();
+        assertEquals(Optional.of("value"), store.get("key"));
+        assertEquals(Optional.of("value"), store.remove("key"));
+        assertEquals(Optional.empty(), store.get("key"));
+
+        store.close();
+
+        store = buildDiskStore();
+        assertEquals(Optional.empty(), store.get("key"));
+        assertEquals(Optional.empty(), store.remove("key"));
+
+        store.close();
+    }
 }
